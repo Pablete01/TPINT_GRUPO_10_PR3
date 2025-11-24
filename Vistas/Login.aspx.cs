5565de8aff80e6670e81eb4a7086f5e65a4ee985
Danilo Leonel Thenoux Pared <danilo.thenoux@alumnos.frgp.utn.edu.ar>
2025-11-23 21:04:00 -0300

    saludo en ramanueva

--- Vistas/Login.aspx.cs
+++ Vistas/Login.aspx.cs
@@ -16,6 +16,7 @@ namespace Vistas
         protected void Page_Load(object sender, EventArgs e)
         {
             ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+            ///hola saludos
         }
 
 
